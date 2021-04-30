5774275a06bf52ab64199943d41a09ab35b1583f
Marufsatir <78980345+Marufsatir@users.noreply.github.com>
2021-04-30 05:37:30 +0300

    Add files via upload

---
+++ htdocs/additionals/HostController.class.php
@@ -0,0 +1,12 @@
+<?php
+class HostController extends Host{
+  //Function that validates the host credentials during Login
+  //@parameter email -> email address of the host
+  //@parameter password -> password of the host
+  final public function validateHost($email, $password)
+  {
+    $this->checkCredentials($email,$password);
+  }
+
+}
+ ?>
