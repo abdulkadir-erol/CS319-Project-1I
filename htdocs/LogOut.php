1af984c7cf9c657eb65887663f10de0fd68d6dbf
Marufsatir <78980345+Marufsatir@users.noreply.github.com>
2021-05-03 23:40:46 +0300

    Delete htdocs directory

--- htdocs/LogOut.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["user"]);
-unset($_SESSION["status"]);
-session_destroy();
-header("Location:	index.php");
-?>
--- htdocs/additionals/HostView.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-class HostView extends Host{
-  
-}
- ?>
